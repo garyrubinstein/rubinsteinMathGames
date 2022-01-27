2bb60dfe06672a3a6bccb6f89fcddbd83875bb6c
Gary Rubinstein <garyrubinstein@yahoo.com>
2022-01-26 22:26:21 -0500

    added stuff for the purchase plus scene it seems to be working a little better but does not have the purchase capability.

--- rubinsteinMathGames/GameViewController.swift
+++ rubinsteinMathGames/GameViewController.swift
@@ -14,10 +14,16 @@ class GameViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        var sceneName = "mainMenu"
+        var plus = false
+        var numGames = 10
+        if (!plus && numGames>9) {
+            sceneName = "purchasePlus"
+        }
         
         if let view = self.view as! SKView? {
             // Load the SKScene from 'GameScene.sks'
-            if let scene = MainMenuScene(fileNamed: "mainMenu") {
+            if let scene = MainMenuScene(fileNamed: sceneName) {// "mainMenu") {
                 // Set the scale mode to scale to fit the window
                 scene.scaleMode = .aspectFit
                 
